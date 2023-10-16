9a34c2fcaaee2fd26e230613f4d0dcc43bbc9af1
hasanbeker2 <hasanbeker22@gmail.com>
2023-10-16 12:54:46 +0200

    the last commitsfor course 5

--- Webscraping_Plotting.py
+++ Webscraping_Plotting.py
@@ -6,9 +6,11 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 def make_graph(stock_data, revenue_data, stock):
+    stock_data['Date'] = pd.to_datetime(stock_data['Date'])
+    revenue_data['Date'] = pd.to_datetime(revenue_data['Date'])
     fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
-    stock_data_specific = stock_data[stock_data.Date <= '2021-06-14']
-    revenue_data_specific = revenue_data[revenue_data.Date <= '2021-04-30']
+    stock_data_specific = stock_data[stock_data['Date'] <= '2021-06-14']
+    revenue_data_specific = revenue_data[revenue_data['Date'] <= '2021-04-30']  
     fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data_specific.Date, infer_datetime_format=True), y=stock_data_specific.Close.astype("float"), name="Share Price"), row=1, col=1)
     fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data_specific.Date, infer_datetime_format=True), y=revenue_data_specific.Revenue.astype("float"), name="Revenue"), row=2, col=1)
     fig.update_xaxes(title_text="Date", row=1, col=1)
@@ -20,3 +22,33 @@ def make_graph(stock_data, revenue_data, stock):
     title=stock,
     xaxis_rangeslider_visible=True)
     fig.show()
+    
+  
+
+# Create a Ticker object for Tesla
+tesla_ticker = yf.Ticker("TSLA")
+
+# Fetch historical market data for Tesla with the maximum amount of time
+tesla_data = tesla_ticker.history(period="max")
+
+# Reset the index
+tesla_data.reset_index(inplace=True)
+
+# Display or check the DataFrame
+print(tesla_data.head())
+
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/revenue.htm"
+html_data = requests.get(url).text
+
+soup = BeautifulSoup(html_data, 'html.parser')
+tables = pd.read_html(html_data)
+tesla_revenue = tables[0]
+tesla_revenue.columns = ['Date', 'Revenue']
+print(tesla_revenue)
+
+tesla_revenue["Revenue"] = tesla_revenue['Revenue'].str.replace(',|\$',"")
+tesla_revenue.dropna(inplace=True)
+tesla_revenue = tesla_revenue[tesla_revenue['Revenue'] != ""]
+
+make_graph(tesla_data, tesla_revenue, 'Tesla')
+
